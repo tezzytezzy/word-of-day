45a70d2e9f25660f25db21af456f5133e44616da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-22 03:26:10 +0000

    Deploy tezzytezzy/word-of-day to github.com/tezzytezzy/word-of-day.git:gh-pages

--- tests.sh
+++ tests.sh
@@ -21,24 +21,23 @@ if [[ $(check_inputs -h) != $(show_help_msg) ]]; then exit 4; fi
 if [[ $(check_inputs "${_non_existent_file}") != $(show_help_msg) ]]; then exit 5; fi
 
 
-if [[ $(check_inputs -f "${_non_existent_file}" --firefox) != $(show_no_filename_msg) ]]; then exit 6; fi
-
-if [[ $(check_inputs --file "${_non_existent_file}" --chrome) != $(show_no_filename_msg) ]]; then exit 7; fi
-
-
-if [[ $(touch "${_zero_size_file}"; check_inputs --file "${_zero_size_file}" --firefox) != $(show_empty_file_msg) ]]; then exit 8; fi
+if [[ $(touch "${_zero_size_file}"; check_inputs --file "${_zero_size_file}" --firefox) != $(show_empty_file_msg) ]]; then exit 6; fi
 rm ${_zero_size_file}
 
-###
-# the following two tests make sure the first line has a number
-export word_list_filename=./english_words.txt
-search_word_in_the_file
-if [[ $(awk 'FNR==1' "${word_list_filename}") != +([[:digit:]]) ]]; then exit 9; fi
 
-export word_list_filename=./german_words.txt
+if [[ $(check_inputs -f "${_non_existent_file}" --firefox) != $(show_no_filename_msg) ]]; then exit 7; fi
+
+if [[ $(check_inputs --file "${_non_existent_file}" --chrome) != $(show_no_filename_msg) ]]; then exit 8; fi
+
+
+export word_list_filename=./english_words.txt
+
+if [[ $(check_inputs --file "${word_list_filename}" --german) != $(show_no_browser_msg) ]]; then exit 9; fi
+
+# Make sure the file has a word line number in Line 1
 search_word_in_the_file
 if [[ $(awk 'FNR==1' "${word_list_filename}") != +([[:digit:]]) ]]; then exit 10; fi
-###
+
 
 # Use ', not ", for literal string!
 selected_word='Heiz\326lr\334cksto\337abd\304mpfung' # Heizölrückstoßabdämpfung
@@ -53,18 +52,18 @@ if [[ $(decode_german_letter "${selected_word}") != "${selected_word}" ]]; then 
 
 if [[ $(launch_browser dummy_word dummy_browser) != $(show_no_browser_installed_msg) ]]; then exit 14; fi
 
-echo test0
+
 browser="${FIREFOX_BROWSER}"
 launch_browser panopticon "${browser}"
 # sleep 5
 # if [[ $(pgrep -c "${browser}") == 0 ]]; then exit 15; fi
 # pkill -f "${browser}"
 # sleep 3
-echo test1
+
 export use_german=0
 launch_browser gemütlichkeit "${CHROME_BROWSER}"
 #sleep 10
-echo finish
+
 # # "chrome" - The middle word of "google-chrome-stable": Bash only string operation
 # chrome_process_name="${CHROME_BROWSER:7:6}"
 # if [[ $(pgrep -c "${chrome_process_name}") == 0 ]]; then exit 16; fi
